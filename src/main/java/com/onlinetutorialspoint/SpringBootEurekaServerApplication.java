4aa0aa36e27d963ee6477320d40415bcf2311d6e
Rahul Gite <imgite@gmail.com>
2022-12-11 14:59:51 +0530

    Initial commit

---
+++ src/main/java/com/onlinetutorialspoint/SpringBootEurekaServerApplication.java
@@ -0,0 +1,14 @@
+package com.onlinetutorialspoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class SpringBootEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEurekaServerApplication.class, args);
+	}
+}
