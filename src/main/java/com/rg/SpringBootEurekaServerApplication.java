39d601f5f38be30374e91dc150622ee63d8b9766
Rahul Gite <imgite@gmail.com>
2022-12-11 15:12:33 +0530

    initial commit

--- src/main/java/com/rg/SpringBootEurekaServerApplication.java
+++ src/main/java/com/rg/SpringBootEurekaServerApplication.java
@@ -1,4 +1,4 @@
-package com.onlinetutorialspoint;
+package com.rg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/rg/SpringBootEurekaServerApplicationTests.java
+++ src/test/java/com/rg/SpringBootEurekaServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.onlinetutorialspoint;
+package com.rg;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
